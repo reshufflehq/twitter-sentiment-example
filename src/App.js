26b445ed928a141723fc6ff5992e06b2a347e66c
zolga <olga.kolopanov@gmail.com>
2019-10-21 15:27:46 -0700

    Support react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Admin from './components/Admin'
+import Admin from './components/Admin/Admin';
+import LiveFrame from './components/LiveFrame';
 import './App.css';
 
 function App() {
   return (
     <Container fluid>
-      <Admin/>
-   </Container>
+      <Router>
+        <Route exact path='/' component={Admin} />
+        <Route path='/live' component={LiveFrame} />
+      </Router>
+    </Container>
   );
 }
 
