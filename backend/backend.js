136beb521c4a2b0d573cbd3213b7ade1386d71f7
zolga <olga.kolopanov@gmail.com>
2019-10-21 15:26:16 -0700

    Code formatting

--- backend/backend.js
+++ backend/backend.js
@@ -1,6 +1,6 @@
- import { update, get } from '@reshuffle/db';
+import { update, get } from '@reshuffle/db';
 
- /**
+/**
  * Save the new http link given from client-side input
  *
  * @param { object } link - http link to cat image, object link store {number} id and {string} url
@@ -9,32 +9,27 @@
  */
 /* @expose */
 export async function addNewUrl(link = {}) {
-  return update("links", (list = []) => list.concat(link));
+  return update('links', (list = []) => list.concat(link));
 }
 
- /**
+/**
  * Delete link from the list by link id
  *
- * @param { number } id - unique id of link 
+ * @param { number } id - unique id of link
  *
  * @return { array } - updated list with all cats links after the link was deleted
  */
 /* @expose */
 export async function deleteLinkById(id) {
-  return update("links", (list = []) =>
-    list.filter(link => link.id !== id)
-  );
+  return update('links', (list = []) => list.filter(link => link.id !== id));
 }
 
-
- /**
+/**
  * List of all cats images urls
  *
- * @return { array } - list with all cats links 
+ * @return { array } - list with all cats links
  */
 /* @expose */
 export async function getLinks() {
-  return (get('links')) || [];
+  return get('links') || [];
 }
-
-
