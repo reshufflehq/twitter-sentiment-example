59aca1231b8ef2d1cd138093e132747eb4d7cc23
zolga <olga.kolopanov@gmail.com>
2019-12-02 23:25:17 -0800

    Add progress bar to result title

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -5,15 +5,32 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import SearchResultItem from '../SearchResultItem/SearchResultItem';
 import SearchResultGridHeaders from '../SearchResultGridHeaders/SearchResultGridHeaders';
+import { Progress } from 'react-sweet-progress';
+import './SearchResult.css';
 
 export default function SearchResult({ result }) {
   const { details, totals } = result;
 
   return (
     <Container>
-      <b>{totals && `User average toxicity level ${totals.tox}%`}</b>
+      <Row className='p-5'>
+        <Col className='level-text text-right pt-1'>
+          {totals && `User average toxicity level: `}
+        </Col>
+        <Col>
+          {totals && (
+            <Progress
+              type='circle'
+              strokeWidth={5}
+              percent={totals.tox}
+              width={100}
+            />
+          )}
+        </Col>
+      </Row>
 
       {details && (
         <Row>
