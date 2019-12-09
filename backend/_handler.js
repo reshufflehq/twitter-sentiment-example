23152453db39f21768b1c8b9961b795dbd9a5ecc
Amir Shevat <amir@reshuffle.com>
2019-12-09 09:38:03 -0800

    removed DB admin

--- backend/_handler.js
+++ backend/_handler.js
@@ -6,8 +6,7 @@ const app = express();
 app.set('trust proxy', true);
 
 // DEV ONLY: uncomment the follow line to admin your local or cloud DB (served this path: /dev-only/db-admin  )
-//devDBAdmin.initDevDBAdmin(app);
-app.use("/dev/db-admin", express.json(), devDBAdmin.devDBAdminHandler);
+//app.use("/dev/db-admin", express.json(), devDBAdmin.devDBAdminHandler);
 
 app.use(defaultHandler);
 
