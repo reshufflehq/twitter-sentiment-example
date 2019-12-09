5cb6574623d5171d282f41fe75f652f4e63157ca
Amir Shevat <amir@reshuffle.com>
2019-12-09 09:27:40 -0800

    added BD admin

--- backend/_handler.js
+++ backend/_handler.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { defaultHandler } from '@reshuffle/server-function';
-const devDBAdmin = require('./_dev_db_admin');
+const devDBAdmin = require('@reshuffle/db-admin');
 
 const app = express();
 app.set('trust proxy', true);
 
 // DEV ONLY: uncomment the follow line to admin your local or cloud DB (served this path: /dev-only/db-admin  )
 //devDBAdmin.initDevDBAdmin(app);
+app.use("/dev/db-admin", express.json(), devDBAdmin.devDBAdminHandler);
 
 app.use(defaultHandler);
 
